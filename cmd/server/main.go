1ef0a5ba67750e61ce2e8414c130dd44c4837acb
Farhan Fansuri <farhanfansuri2023@gmail.com>
2025-12-04 09:03:03 +0700

    add: implement database connection and user model

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"golang-ex/internal/config"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Application entry point
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	// Database connection would be initialized here
+	config.ConnectDatabaseMySQL()
+}
--- internal/config/database.go
+++ internal/config/database.go
@@ -2,13 +2,41 @@ package config
 
 import (
 	"fmt"
+	"os"
+
+	"golang-ex/internal/models"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var (
-	DB *gorm.DB
+	DB  *gorm.DB
+	err error
 )
 
-func ConnectDatabaseMySQL(user, password, host, dbname string) error {
+func ConnectDatabaseMySQL() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic("failed to connect database")
+	} else {
+		fmt.Println("database connected")
+	}
+
+	err = DB.AutoMigrate(&models.User{})
+	if err != nil {
+		panic("failed to migrate database")
+	} else {
+		fmt.Println("Auto migrations")
+	}
+}
